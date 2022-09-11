f6f1f97b969a2624741d38b5f14ae7515110bc56
Michael Ren <michaelren0129@gmail.com>
2022-09-11 17:50:10 +1200

    Fixed attraction

--- game.js
+++ game.js
@@ -8,24 +8,23 @@ class Grid {
         this.height = h;
         this.nodeSize = 80;
         this.nodes = [];
+        this.bars = [];
+        this.magnets = [];
 
         this.populate();
-        let magnets = [];
 
         for (let i=0; i<randomInt(1, 5); i++) {
-            magnets.push(this.setMagnet(randomInt(0, this.width-1), randomInt(0, this.height-1)));
+            this.setMagnet(randomInt(0, this.width-1), randomInt(0, this.height-1));
         }
-        magnets.forEach(function(item) {
-            item.attract();
-        });
-
     }
     
     populate() {
         for (let y=0; y<this.height; y++) {
             this.nodes.push([]);
             for (let x=0; x<this.width; x++) {
-                this.nodes[y][x] = new Bar(this, x, y);
+                let bar = new Bar(this, x, y);
+                this.nodes[y][x] = bar;
+                this.bars.push(bar);
             }  
         }
     }
@@ -34,6 +33,7 @@ class Grid {
         if ((0 <= x && x < this.width) && (0 <= y && y < this.height)) {
             let magnet = new Magnet(this, x, y);
             this.nodes[y][x] = magnet;
+            this.magnets.push(magnet);
             return magnet;
         }
         return undefined;
@@ -68,21 +68,77 @@ class Bar {
         this.width = 20;
         this.height = 60;
         // 8 rotations: 0(North) - 7 clockwise
-        this.rotation = 0;
+        this.rotation = randomInt(0, 7);
+        this.targetRotation = null;
     }
 
-    // getNeighbours() {
-    //     let nodeList = [];
-    //     let relativePositions = [[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,-1],[-1,0],[-1,-1]];
-    //     relativePositions.forEach(function(item) {
-    //         let absolutePosition = [this.x + item[0], this.y + item[1]];
-    //         let node = this.grid.locate(...absolutePosition);
-    //         if (node) {
-    //             nodeList.push(node);
-    //         }
-    //     });
-    //     return nodeList;
-    // }
+    getNeighbours() {
+        let nodeList = [];
+        let relativePositions = [[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]];
+        relativePositions.forEach(pos => {
+            let absolutePosition = [this.x + pos[0], this.y + pos[1]];
+            let node = this.grid.locate(...absolutePosition);
+            if (node) {
+                nodeList.push(node);
+            }
+        });
+        return nodeList;
+    }
+
+    attract() {
+        let rotation = 0;
+        let magnetCount = 0;
+        let magDirections = [];
+
+        let nodeList = this.getNeighbours();
+        nodeList.forEach(item => {
+            if (item.constructor.name === "Magnet") {
+                let offset = [item.x - this.x, item.y - this.y];
+                let direction;
+                switch(JSON.stringify(offset)) {
+                    case "[0,-1]":
+                        direction = 0;
+                        break;
+                    case "[1,-1]":
+                        direction = 1;
+                        break;
+                    case "[1,0]":
+                        direction = 2;
+                        break;
+                    case "[1,1]":
+                        direction = 3;
+                        break;
+                    case "[0,1]":
+                        direction = 4;
+                        break;
+                    case "[-1,1]":
+                        direction = 5;
+                        break;
+                    case "[-1,0]":
+                        direction = 6;
+                        break;
+                    case "[-1,-1]":
+                        direction = 7;
+                        break;
+                }
+                rotation += direction;
+                magDirections.push(direction);
+                magnetCount++;
+            }
+        });
+
+        if (magnetCount) {
+            let totalRotation = rotation / magnetCount;
+
+            if (totalRotation % 1 !== 0) {
+                let nearDirections = [0, 2, 4, 6].filter(v => magDirections.includes(v));
+                totalRotation = nearDirections.reduce(function(prev, curr) {
+                    return (Math.abs(curr - totalRotation) < Math.abs(prev - totalRotation) ? curr : prev);
+                });
+            }
+            this.rotation = totalRotation;
+        }
+    }
 
     setRotation(n) {
         // 8 possible options for n (0 - 7)
@@ -103,7 +159,7 @@ class Bar {
         fill(0);
         rect(0,0, this.width, this.height, 20);
         fill(255);
-        circle(0, 0-circleYOffset+circleRadius, circleRadius);
+        // circle(0, 0-circleYOffset+circleRadius, circleRadius);
         // Reset rotation and translation
         rotate(-rotation);
         translate(-renderX, -renderY);
@@ -116,54 +172,6 @@ class Magnet {
         this.x = x;
         this.y = y;
         this.radius = 30;
-    }
-
-    getNeighbours() {
-        let nodeList = [];
-        let relativePositions = [[0,-1],[1,-1],[1,0],[1,1],[0,1],[-1,1],[-1,0],[-1,-1]];
-        relativePositions.forEach(pos => {
-            let absolutePosition = [this.x + pos[0], this.y + pos[1]];
-            let node = this.grid.locate(...absolutePosition);
-            if (node) {
-                nodeList.push(node);
-            }
-        });
-        return nodeList;
-    }
-
-    attract() {
-        let nodeList = this.getNeighbours();
-        nodeList.forEach(item => {
-            if (item.constructor.name === "Bar") {
-                let offset = [item.x - this.x, item.y - this.y];
-                switch(JSON.stringify(offset)) {
-                    case "[0,-1]":
-                        item.setRotation(4);
-                        break;
-                    case "[1,-1]":
-                        item.setRotation(5);
-                        break;
-                    case "[1,0]":
-                        item.setRotation(6);
-                        break;
-                    case "[1,1]":
-                        item.setRotation(7);
-                        break;
-                    case "[0,1]":
-                        item.setRotation(0);
-                        break;
-                    case "[-1,1]":
-                        item.setRotation(1);
-                        break;
-                    case "[-1,0]":
-                        item.setRotation(2);
-                        break;
-                    case "[-1,-1]":
-                        item.setRotation(3);
-                        break;
-                }
-            }
-        });
     }
 
     draw() {
@@ -183,6 +191,9 @@ function setup() {
     rectMode(CENTER);
     noStroke(); 
     grid = new Grid(5, 5);
+    grid.bars.forEach(function(item) {
+        item.attract();
+    })
 }
 
 function draw() {
